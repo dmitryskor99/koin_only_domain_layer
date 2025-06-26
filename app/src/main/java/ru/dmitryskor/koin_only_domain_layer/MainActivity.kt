90a4a6e045216048407e65fee422c6698df6b398
Dmitrii.Skorodumov <skorodumov@skbkontur.ru>
2025-06-26 17:21:39 +0300

    commint android app module

---
+++ app/src/main/java/ru/dmitryskor/koin_only_domain_layer/MainActivity.kt
@@ -0,0 +1,47 @@
+package ru.dmitryskor.koin_only_domain_layer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ru.dmitryskor.koin_only_domain_layer.ui.theme.Koin_only_domain_layerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Koin_only_domain_layerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Koin_only_domain_layerTheme {
+        Greeting("Android")
+    }
+}
